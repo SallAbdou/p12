675f0e9552e47f05f0f9b35433996506a4145f4b
Abdou <sall.abdou34@gmail.com>
2024-07-26 14:51:36 +0200

    css user page

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -8,6 +8,7 @@ import { getActivityByUserId, getAverageSessionsByUserId, getUserByUserId, getPe
 import { useGet } from '../utils/hooks';
 import Keydata from '../components/Keydata'
 
+
 function UserPage() {
   const { id } = useParams();
   const userActivity = useGet(getActivityByUserId(id));
@@ -29,28 +30,32 @@ function UserPage() {
           <span>{userInfo.data.firstName}</span>
           <div>Félicitation ! Vous avez explosé vos objectifs hier 👏</div>
         </div>
-        <div className="charts-layout">
-          <div className="charts">
-            <div className='activity'>
-              <ActivityChart data={userActivity.data.sessions} />
+      
+        <div className='charts-and-data'>
+            <div className="charts-layout">
+              <div className="charts">
+                <div className='activity'>
+                  <ActivityChart data={userActivity.data.sessions} />
+                </div>
+                <div className="average-sessions">
+                  <span>Durée moyenne des sessions</span>
+                  <AverageSessionsChart data={userAverageSessions.data.sessions} />
+                </div>
+                <div className="performance-chart">
+                  <PerformanceChart data={userPerformance.data} />
+                </div>
+                <div className="objectif">
+                  <span>Objectif</span>
+                  <ObjectifChart data={userInfo.data.score} />
+                </div>
+              </div>
             </div>
-            <div className="average-sessions">
-              <span>Durée moyenne des sessions</span>
-              <AverageSessionsChart data={userAverageSessions.data.sessions} />
+            <div className="key-data">
+              {userInfo.isLoading ? <div>loading...</div> : userInfo?.data.keyDatas.map(item => <Keydata key={item.name} data={item} />)}
             </div>
-            <div className="performance-chart">
-              <PerformanceChart data={userPerformance.data} />
-            </div>
-            <div className="objectif">
-              <span>Objectif</span>
-              <ObjectifChart data={userInfo.data.score} />
-            </div>
-          </div>
-        </div>
-        <div className="key-data">
-          {userInfo.isLoading ? <div>loading...</div> : userInfo?.data.keyDatas.map(item => <Keydata key={item.name} data={item} />)}
-        </div>
-      </div>
+       </div>
+     
+      </div>   
 
     </section>
   );
